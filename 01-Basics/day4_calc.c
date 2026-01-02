78ee943214e9942f50a31ae2b7afe39e32ecf109
Laxmi A Sanas <sanasaarati@gmail.com>
2026-01-02 13:23:55 +0530

    Add basic calculator in day4_calc.c

---
+++ 01-Basics/day4_calc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a = 10, b = 5;
+    printf("Sum: %d\n", a + b);
+    printf("Diff: %d\n", a - b);
+    printf("Prod: %d\n", a * b);
+    printf("Div: %d\n", a / b);
+    return 0;
+}
